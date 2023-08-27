d3423f69293e43b627c0591209ad8d07f54cdb72
NMcHale029 <noah.reetz@gmail.com>
2023-08-27 17:47:12 -0500

    Homework Commit

--- EmployeeSQL/Employee_schema.sql
+++ EmployeeSQL/Employee_schema.sql
@@ -24,7 +24,7 @@ CREATE TABLE "employees" (
 CREATE TABLE "departments" (
     "dept_no" VARCHAR(30)PRIMARY KEY NOT NULL,
     "dept_name" VARCHAR(30) NOT NULL
-     );
+);
 
 CREATE TABLE "dept_emp" (
     "emp_no" int NOT NULL,
@@ -40,13 +40,11 @@ CREATE TABLE "dept_manager" (
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
-
 CREATE TABLE "salaries" (
     "emp_no" int NOT NULL,
     "salary" int NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
-
 
 SELECT * FROM titles;
 SELECT * FROM employees;
