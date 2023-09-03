325d03b0f96b7719e5ece45677bc3a06f1520103
NMcHale029 <noah.reetz@gmail.com>
2023-09-03 08:59:49 -0500

    corrections to homework

--- EmployeeSQL/Employee_schema.sql
+++ EmployeeSQL/Employee_schema.sql
@@ -29,6 +29,7 @@ CREATE TABLE "departments" (
 CREATE TABLE "dept_emp" (
     "emp_no" int NOT NULL,
     "dept_no" VARCHAR(30) NOT NULL,
+	PRIMARY KEY (emp_no, dept_no),
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
@@ -36,13 +37,14 @@ CREATE TABLE "dept_emp" (
 CREATE TABLE "dept_manager" (
     "dept_no" VARCHAR(30) NOT NULL,
     "emp_no" int NOT NULL,
+	PRIMARY KEY (dept_no, emp_no)
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
 
 CREATE TABLE "salaries" (
-    "emp_no" int NOT NULL,
+    "emp_no" int PRIMARY KEY NOT NULL,
     "salary" int NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
